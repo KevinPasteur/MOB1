57dd5e4f0e4cc318a5bab0d332aff662dee29214
Kevin <kevin.pasteur@cpnv.ch>
2020-05-05 08:37:43 +0200

    Update homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public welcomeMessage : string = 'Hello le monde';
+  public name;
 
   constructor() {}
 
