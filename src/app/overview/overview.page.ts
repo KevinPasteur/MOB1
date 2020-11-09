77a232519e74b0dd48f84ad2ef17ab09f5be4dd0
Kevin <kevin.pasteur@cpnv.ch>
2020-11-09 10:40:22 +0100

    Add updateStock (Hardcoded)

--- src/app/overview/overview.page.ts
+++ src/app/overview/overview.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+import { DataProvider } from '../providers/data';
 
 @Component({
   selector: 'app-overview',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewPage implements OnInit {
 
-  constructor() { }
+  id:any
+  overviewArray = []
+  constructor(private data: DataProvider,private router: Router,private route: ActivatedRoute, private http: HttpClient,private toaster: ToastController) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.overviewArray.push(params)
+      console.log(this.overviewArray)
+      this.overviewArray.forEach((element) =>{
+        console.log(element[0])
+      })
+
+      this.listOfVegs.forEach((element,index) => {
+          if(element.name == this.vegFound['name']){
+            this.listOfVegs.splice(index, 1);
+          }
+        })
+
+    });
+
   }
 
+  updateStock(){
+    this.data.updateStock()
+  }
+
+  goToStock(){
+    this.router.navigate(['/stock']);
+  }
 }
--- src/app/providers/data.ts
+++ src/app/providers/data.ts
@@ -8,81 +8,122 @@ import { HTTP } from '@ionic-native/http/ngx';
 @Injectable()
 export class DataProvider {
 
-    private apiurl: string = 'http://127.0.0.1:8000/api'
-    public stock = []
-    public user = []
-    public info = []
-    public error:string;
-    public balance:any = [];
+  private apiurl: string = 'http://127.0.0.1:8000/api'
+  public stock = []
+  public user = []
+  public info = []
+  public error:string;
+  public balance:any = [];
 
-    private token =''
-    
-    constructor(private router: Router,private http: HttpClient,private storage: Storage,private toaster: ToastController){}
+  private token =''
+  
+  constructor(private router: Router,private http: HttpClient,private storage: Storage,private toaster: ToastController){}
 
-    public loadFromAPI(): Promise<any>
-    {
+  public loadFromAPI(): Promise<any>
+  {
+    return new Promise<any>((resolve,reject) => {
+      this.http.get(this.apiurl+'/products').subscribe(
+        response => {
+          this.stock = response["data"];
+          resolve(this.stock)
+        },
+        err => {
+          this.error = "Un problème est survenu, veuillez vérifier votre connexion ou contacter le webmaster."
+        }
+      )
+    })
+  }
+
+public checkUser(): Promise<any>
+  {
       return new Promise<any>((resolve,reject) => {
-        this.http.get(this.apiurl+'/products').subscribe(
-          response => {
-            this.stock = response["data"];
-            resolve(this.stock)
-          },
-          err => {
-            this.error = "Un problème est survenu, veuillez vérifier votre connexion ou contacter le webmaster."
-          }
-        )
-      })
-    }
+      this.http.get(this.apiurl+'/me')
+      .subscribe( response => {
+        this.user = response["data"]
+        resolve(this.user)
+        },
+        err => {
+          this.toaster.create({
+            message: 'Votre token est invalide. Veuillez réessayer !',
+            duration: 4000,
+            color: 'danger'
+          }).then(toast => {toast.present()})
+          this.storage.remove('token');
+        }
+      )
+    })
+    
+  }
 
-  public checkUser(): Promise<any>
-    {
-       return new Promise<any>((resolve,reject) => {
-        this.http.get(this.apiurl+'/me')
-        .subscribe( response => {
-          this.user = response["data"]
-          resolve(this.user)
-          },
-          err => {
-            this.toaster.create({
-              message: 'Votre token est invalide. Veuillez réessayer !',
-              duration: 4000,
-              color: 'danger'
-            }).then(toast => {toast.present()})
-            this.storage.remove('token');
-          }
-        )
-      })
-     
-    }
-
-    public find(id){
-      return new Promise<any>((resolve, reject) => {
+  public find(id){
+    return new Promise<any>((resolve, reject) => {
+      
+      this.stock.forEach((ved) =>{
         
-        this.stock.forEach((ved) =>{
-          
-          if (ved.id == id) resolve(ved)
-        })
-        reject('Vedj # ' + id + ' not found')
+        if (ved.id == id) resolve(ved)
       })
+      reject('Vedj # ' + id + ' not found')
+    })
+  }
+
+  public getUserBalance(): Promise<any> {
+    return new Promise<any> ( (resolve, reject) => {
+      this.http.get(this.apiurl+"/me/balance").subscribe(
+        response => {
+          this.balance = response;
+          resolve(this.balance);
+        },
+        err => {
+          this.toaster.create({
+            message: 'Votre token est invalide. Veuillez réessayer !',
+            duration: 4000,
+            color: 'danger'
+          }).then(toast => {toast.present()})
+          this.storage.remove('token');
+        }
+      );
+    })
+  }
+  
+  public updateStock()
+  {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+      
+    });
+
+    let options = {
+      headers: headers
     }
 
-    public getUserBalance(): Promise<any> {
-      return new Promise<any> ( (resolve, reject) => {
-        this.http.get(this.apiurl+"/me/balance").subscribe(
-          response => {
-            this.balance = response;
-            resolve(this.balance);
-          },
-          err => {
-            this.toaster.create({
-              message: 'Votre token est invalide. Veuillez réessayer !',
-              duration: 4000,
-              color: 'danger'
-            }).then(toast => {toast.present()})
-            this.storage.remove('token');
-          }
-        );
-      })
-    }
+    let erreur = "";
+
+    let data = { 
+      "quantities" : [{ 
+        "id": 1, "quantity": 10
+        }, { 
+        "id": 2, "quantity": 2
+        }] 
+      }
+
+     this.http.post(this.apiurl+'/products/stock', data, options)
+    .subscribe(
+      data => {this.toaster.create({
+        message: 'Le Stock a bien été modifié',
+        duration: 4000,
+        color: 'success'
+      }).then(toast => {toast.present()})
+      },
+      err => {
+        this.toaster.create({  
+          message: erreur,
+          duration: 2000,
+          color: 'danger'
+      }).then(toast => {toast.present()})
+      }
+      
+    )
+
+  }
 
 }
